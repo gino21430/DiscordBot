e6b86074270d8ef2ee5c0d54ee044375275c6276
gino21430 <gino21430@gmail.com>
2018-08-07 13:11:58 +0800

    完成狀態監控

--- src/main/java/me/monica/cat/discordbot/handler/ServerStatusHandler.java
+++ src/main/java/me/monica/cat/discordbot/handler/ServerStatusHandler.java
@@ -48,11 +48,15 @@ public class ServerStatusHandler {
 
     private String getData() {
         Runtime lRuntime = Runtime.getRuntime();
+        OperatingSystemMXBean osmb = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        java.lang.management.ThreadMXBean t = ManagementFactory.getThreadMXBean();
+        Date date = new Date();
+
+        //Memory
         int aM = (int) lRuntime.freeMemory() / 1024 / 1024;
         int mM = (int) lRuntime.maxMemory() / 1024 / 1024;
         int tM = (int) lRuntime.totalMemory() / 1024 / 1024;
-        OperatingSystemMXBean osmb = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-        java.lang.management.ThreadMXBean t = ManagementFactory.getThreadMXBean();
+
         //cpu
         DecimalFormat df = new DecimalFormat("#.##");
         String cpu = df.format(osmb.getSystemCpuLoad() * 100);
@@ -69,26 +73,18 @@ public class ServerStatusHandler {
             synchronized (this) {
                 double tmp = System.currentTimeMillis() - startTime;
                 tps = 10.0 * 1000 / tmp;
-                if (tps > 20.0) tps = 20.0;
+                if (tps > 20.0) tps = 20.00;
             }
         }, 10L);
 
-        return color(t.getThreadCount(), t.getDaemonThreadCount(), t.getPeakThreadCount(),
-                tM, aM, mM, cpu, players, chunks);
-    }
-
-    private String color(int t1, int t2, int t3, int tM, int aM, int mM, String cpu, int players, int chunks) {
-        Date date = new Date();
-        String info = "==========" + date.toString() + "==========" + "\n" +
-                "spigot/jvm/max : %s/%s/%s\n" +
-                "Memory (total/available/max)：%s/%s/%s MB" + "\n" +
-                "CPU : %s %" + "\n" +
-                "Players : %s\n" +
-                "Chunks : %s\n" +
-                "TPS : %s\n" +
+        return "==========" + date.toString() + "==========" + "\n" +
+                "Thread (spigot/jvm/max) : " + t.getThreadCount() + "/" + t.getDaemonThreadCount() + "/" + t.getPeakThreadCount() + "\n" +
+                "Memory (total/available/max)：" + tM + "/" + aM + "/" + mM + " MB" + "\n" +
+                "CPU : " + cpu + " %" + "\n" +
+                "Players : " + players + "\n" +
+                "Chunks : " + chunks + "\n" +
+                "TPS : " + tps + "\n" +
                 "==========================================";
-        //if (tM > mM * 0.8)
-        return String.format(info, t1, t2, t3);
     }
 
 }
